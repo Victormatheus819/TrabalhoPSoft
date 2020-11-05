0818ec909792b3075dc5da0522800c7acf06079c
VictorMatheus819 <victormatheus819@gmail.com>
2020-11-05 20:58:39 -0300

    limpeza de repositorio

--- src/main/java/com/example/teste/TesteApplication.java
+++ src/main/java/com/example/teste/TesteApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TesteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SuperTest.class, args);
+		SpringApplication.run(TesteApplication.class, args);
 		
 		
 	}
