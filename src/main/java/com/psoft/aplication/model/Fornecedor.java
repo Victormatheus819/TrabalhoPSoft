f6ae2dec0391475eab79e00512d88b16d8ebd3ed
VictorMatheus819 <victormatheus819@gmail.com>
2020-11-12 19:44:08 -0300

    fornecedor reformulado

--- src/main/java/com/psoft/aplication/model/Fornecedor.java
+++ src/main/java/com/psoft/aplication/model/Fornecedor.java
@@ -1,11 +1,14 @@
 package com.psoft.aplication.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
-
+@Entity
+@Table (name="fornecedor")
 public class Fornecedor {
 
     @Id
