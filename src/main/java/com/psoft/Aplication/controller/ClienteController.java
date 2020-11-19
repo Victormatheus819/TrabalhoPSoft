9dd405e398f48aa61475661da76ebc959655136d
VictorMatheus819 <victormatheus819@gmail.com>
2020-11-18 21:08:09 -0300

    inserção no banco

---
+++ src/main/java/com/psoft/Aplication/controller/ClienteController.java
@@ -0,0 +1,28 @@
+package com.psoft.aplication.controller;
+
+import com.psoft.aplication.dao.ClienteDao;
+import com.psoft.aplication.model.Cliente;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ClienteController {
+    @Autowired
+    private ClienteDao dao;
+    
+    @RequestMapping(value = "/teste", method = RequestMethod.GET)
+    public String form(){
+        return "index";  
+    }
+    
+    @RequestMapping(value = "/teste", method = RequestMethod.POST)
+    public String form(Cliente cliente){
+        System.out.println(cliente.getEndereco());
+        System.out.println (cliente.isPeferencial());
+        dao.save(cliente);
+        return "index";  
+    } 
+}
